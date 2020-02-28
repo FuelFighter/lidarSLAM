31fc93717046e29e670256e9937992ab14e660ff
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-28 17:38:39 +0100

    uploaded new test file

--- ouster_scripts/test.sh
+++ ouster_scripts/test.sh
@@ -1,13 +1,7 @@
 source ~/catkin2_ws/src/cartographer_ws/install_isolated/setup.bash
-#gnome-terminal -e "bash -c \"roslaunch cartographer_ros sample_rviz.launch bag_filename:=${HOME}/ouster_scripts/2020-02-14-09-55-32.bag; exec bash\""
-#gnome-terminal -e "bash -c \"roslaunch cartographer_ros racer_2d_cartographer1.launch; exec bash\""
-#^launcher den første :)))))
 
-#gnome-terminal -e "bash -c \"roslaunch cartographer_ros racer_2d_cartographer.launch bag_filenames:=${HOME}/ouster_scripts/2020-02-14-09-55-32.bag; exec bash\""
-#^launcher den andre dingsen. 
-#sleep 3
-#gnome-terminal -e "bash -c \"roslaunch cartographer_ros racer_2d_cartographer.launch bag_filenames:=${HOME}/ouster_scripts/2020-02-14-09-55-32.bag; exec bash\""
-gnome-terminal -e "bash -c \"roslaunch cartographer_ros racer_2d_cartographer1.launch bag_filenames:=${HOME}/ouster_scripts/2020-02-14-09-55-32.bag; exec bash\""
-#gnome-terminal -e "bash -c \"rosbag play -l ~/ouster_scripts/2020-02-14-09-55-32.bag tf_old:=tf; exec bash\""
+gnome-terminal -e "bash -c \"roslaunch cartographer_ros racer_2d_cartographer.launch; exec bash\""
 
-#gnome-terminal -e "bash -c \"rosbag play -l ~/ouster_scripts/2020-02-14-09-55-32.bag; exec bash\""
+sleep 3
+
+gnome-terminal -e "bash -c \"rosbag play ~/ouster_scripts/all_topics_lidar.filtered.bag tf_old:=tf --clock; exec bash\""
